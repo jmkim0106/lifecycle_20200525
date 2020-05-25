79d963eadf6e0c125d062d96a2b049c046ecebef
weekyeon <weekyeon@gmail.com>
2020-05-25 15:09:12 +0900

    onResume 로그 확인

--- app/src/main/java/com/example/lifecycle/MainActivity.java
+++ app/src/main/java/com/example/lifecycle/MainActivity.java
@@ -25,6 +25,13 @@ public class MainActivity extends BaseActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d("생명주기확인","onResume실행");
+    }
+
+    @Override
     public void setUpEvents() {
 
         binding.moveBtn.setOnClickListener(new View.OnClickListener() {
